6c50c73f39eda505333554970988023f6eb7dd04
Burak Özkan <burak.ozkan@dokabilisim.com>
2021-04-13 20:35:01 +0300

    BE ready for FE

---
+++ routes/postRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import {getAllPosts} from "../controllers/postControllers.js";
+
+const router = express.Router();
+
+router.route('/').get(getAllPosts);
+
+export default router
--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 // custom components
 import connectDB from "./config/db.js"
+// routers
+import postRoutes from "./routes/postRoutes.js";
 
 connectDB();
 
 dotenv.config(); 
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/posts', postRoutes)
 
 const PORT = process.env.PORT;
 
